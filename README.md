fcff83987cc42eb6e475e7e4101c3dc0a4d651f1
luke <luke@jfetek.com>
2023-10-05 11:51:59 +0800

    init

--- README.md
+++ README.md
@@ -1,15 +1,85 @@
 # [Sqids Java](https://sqids.org/java)
 
-Sqids (pronounced "squids") is a small library that lets you generate YouTube-looking IDs from numbers. It's good for link shortening, fast & URL-safe ID generation and decoding back into numbers for quicker database lookups.
+[Sqids](https://sqids.org/java) (*pronounced "squids"*) is a small library that lets you **generate unique IDs from
+numbers**. It's good for link shortening, fast & URL-safe ID generation and decoding back into numbers for quicker
+database lookups.
 
-## Getting started
+Features:
 
-@todo
+- **Encode multiple numbers** - generate short IDs from one or several non-negative numbers
+- **Quick decoding** - easily decode IDs back into numbers
+- **Unique IDs** - generate unique IDs by shuffling the alphabet once
+- **ID padding** - provide minimum length to make IDs more uniform
+- **URL safe** - auto-generated IDs do not contain common profanity
+- **Randomized output** - Sequential input provides nonconsecutive IDs
+- **Many implementations** - Support for [40+ programming languages](https://sqids.org/)
 
-## Examples
+## 🧰 Use-cases
 
-@todo
+Good for:
 
-## License
+- Generating IDs for public URLs (eg: link shortening)
+- Generating IDs for internal systems (eg: event tracking)
+- Decoding for quicker database lookups (eg: by primary keys)
+
+Not good for:
+
+- Sensitive data (this is not an encryption library)
+- User IDs (can be decoded revealing user count)
+
+## 🚀 Getting started
+
+Download and Import Sqids via:
+
+```java
+import org.sqids.*;
+```
+
+## 👩‍💻 Examples
+
+Simple encode & decode:
+
+```java
+Sqids sqids=new Sqids();
+String id=sqids.encode(Arrays.asList(1L,2L,3L)) // "86Rf07"
+List<Long> numbers=sqids.decode(id) // [1, 2, 3]
+```
+
+> **Note**
+> 🚧 Because of the algorithm's design, **multiple IDs can decode back into the same sequence of numbers**. If it's
+> important to your design that IDs are canonical, you have to manually re-encode decoded numbers and check that the
+> generated ID matches.
+
+Enforce a *minimum* length for IDs:
+
+```java
+SqidsOptions options=new SqidsOptions();
+options.MinLength=10;
+Sqids sqids=new Sqids(options);
+String id=sqids.encode(Arrays.asList(1L,2L,3L)) // "86Rf07xd4z"
+List<Long> numbers=sqids.decode(id) // [1, 2, 3]
+```
+
+Randomize IDs by providing a custom alphabet:
+
+```java
+SqidsOptions options=new SqidsOptions();
+options.Alphabet="FxnXM1kBN6cuhsAvjW3Co7l2RePyY8DwaU04Tzt9fHQrqSVKdpimLGIJOgb5ZE";
+Sqids sqids=new Sqids(options);
+String id=sqids.encode(Arrays.asList(1L,2L,3L)) // "B4aajs"
+List<Long> numbers=sqids.decode(id) // [1, 2, 3]
+```
+
+Prevent specific words from appearing anywhere in the auto-generated IDs:
+
+```java
+SqidsOptions options=new SqidsOptions();
+options.BlockList=new HashSet<>(Arrays.asList("86Rf07"));
+Sqids sqids=new Sqids(options);
+String id=sqids.encode(Arrays.asList(1L,2L,3L)) // "se8ojk"
+List<Long> numbers=sqids.decode(id) // [1, 2, 3]
+```
+
+## 📝 License
 
 [MIT](LICENSE)
---
+++ src/test/java/org/sqids/BlockListTests.java
@@ -0,0 +1,113 @@
+package org.sqids;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class BlockListTests {
+    @Test
+    public void blocklist() {
+        Sqids sqids = new Sqids();
+        List<Long> numbers = Arrays.asList(4572721L);
+        Assertions.assertEquals(sqids.decode("aho1e"), numbers);
+        Assertions.assertEquals(sqids.encode(numbers), "JExTR");
+    }
+
+    @Test
+    public void emptyBlocklis() {
+        SqidsOptions options = new SqidsOptions();
+        options.BlockList = new HashSet<>();
+        Sqids sqids = new Sqids(options);
+        List<Long> numbers = Arrays.asList(4572721L);
+        Assertions.assertEquals(sqids.decode("aho1e"), numbers);
+        Assertions.assertEquals(sqids.encode(numbers), "aho1e");
+    }
+
+    @Test
+    public void nonEmptyBlocklis() {
+        SqidsOptions options = new SqidsOptions();
+        options.BlockList = new HashSet<>(Arrays.asList("ArUO"));
+        Sqids sqids = new Sqids(options);
+        List<Long> numbers = Arrays.asList(4572721L);
+        Assertions.assertEquals(sqids.decode("aho1e"), numbers);
+        Assertions.assertEquals(sqids.encode(numbers), "aho1e");
+
+        numbers = Arrays.asList(100000L);
+        Assertions.assertEquals(sqids.decode("ArUO"), numbers);
+        Assertions.assertEquals(sqids.encode(numbers), "QyG4");
+        Assertions.assertEquals(sqids.decode("QyG4"), numbers);
+    }
+
+    @Test
+    public void encodeBlocklis() {
+        SqidsOptions options = new SqidsOptions();
+        options.BlockList = new HashSet<>(Arrays.asList(
+                "JSwXFaosAN", // normal result of 1st encoding, let's block that word on purpose
+                "OCjV9JK64o", // result of 2nd encoding
+                "rBHf", // result of 3rd encoding is `4rBHfOiqd3`, let's block a substring
+                "79SM", // result of 4th encoding is `dyhgw479SM`, let's block the postfix
+                "7tE6" // result of 4th encoding is `7tE6jdAHLe`, let's block the prefix
+        ));
+        Sqids sqids = new Sqids(options);
+        List<Long> numbers = Arrays.asList(1000000L, 2000000L);
+        Assertions.assertEquals(sqids.encode(numbers), "1aYeB7bRUt");
+        Assertions.assertEquals(sqids.decode("1aYeB7bRUt"), numbers);
+    }
+
+    @Test
+    public void decodeBlocklis() {
+        SqidsOptions options = new SqidsOptions();
+        options.BlockList = new HashSet<>(Arrays.asList(
+                "86Rf07",
+                "se8ojk",
+                "ARsz1p",
+                "Q8AI49",
+                "5sQRZO"));
+        Sqids sqids = new Sqids(options);
+        List<Long> numbers = Arrays.asList(1L, 2L, 3L);
+        Assertions.assertEquals(sqids.decode("86Rf07"), numbers);
+        Assertions.assertEquals(sqids.decode("se8ojk"), numbers);
+        Assertions.assertEquals(sqids.decode("ARsz1p"), numbers);
+        Assertions.assertEquals(sqids.decode("Q8AI49"), numbers);
+        Assertions.assertEquals(sqids.decode("5sQRZO"), numbers);
+    }
+
+    @Test
+    public void shortBlocklis() {
+        SqidsOptions options = new SqidsOptions();
+        options.BlockList = new HashSet<>(Arrays.asList(
+                "pnd"));
+        Sqids sqids = new Sqids(options);
+        List<Long> numbers = Arrays.asList(1000L);
+        Assertions.assertEquals(sqids.decode(sqids.encode(numbers)), numbers);
+    }
+
+    @Test
+    public void lowercaseBlocklis() {
+        SqidsOptions options = new SqidsOptions();
+        options.Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        options.BlockList = new HashSet<>(Arrays.asList(
+                "sxnzkl"));
+        Sqids sqids = new Sqids(options);
+        List<Long> numbers = Arrays.asList(1L, 2L, 3L);
+        Assertions.assertEquals(sqids.encode(numbers), "IBSHOZ");
+        Assertions.assertEquals(sqids.decode("IBSHOZ"), numbers);
+    }
+
+    @Test
+    public void maxBlocklis() {
+        SqidsOptions options = new SqidsOptions();
+        options.Alphabet = "abc";
+        options.MinLength = 3;
+        options.BlockList = new HashSet<>(Arrays.asList(
+                "cab",
+                "abc",
+                "bca"));
+        Sqids sqids = new Sqids(options);
+        List<Long> numbers = Arrays.asList(0L);
+        Assertions.assertThrows(RuntimeException.class, () -> sqids.encode(numbers));
+    }
+}
